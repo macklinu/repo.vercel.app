30cb9a3ae4a7d5c1f3b12a3d63d7acdef095bebd
mackie <macklinu@users.noreply.github.com>
2022-07-31 13:16:27 -0400

    Update ESLint config

--- lib/parseRepoUrl.test.ts
+++ lib/parseRepoUrl.test.ts
@@ -8,7 +8,10 @@ test.each([
     'https://github.com/babel/babel/tree/master/packages/babel-cli',
     'https://github.com/babel/babel/tree/master/packages/babel-cli',
   ],
-  ['git+https://github.com/zeit/ncc.git', 'https://github.com/zeit/ncc.git'],
+  [
+    'git+https://github.com/vercel/ncc.git',
+    'https://github.com/vercel/ncc.git',
+  ],
 ])('parseRepoUrl(%p) => %p', (input, expected) => {
   expect(parseRepoUrl(input)).toEqual(expected)
 })
