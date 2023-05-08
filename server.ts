11af1fe03c1108747ee2ff905b1371201bd70f18
mackie <macklinu@users.noreply.github.com>
2023-05-08 12:55:42 -0400

    Install vercel/remix

--- server.ts
+++ server.ts
@@ -1,7 +1,15 @@
-import { createRequestHandler } from '@remix-run/vercel'
+import { createRequestHandler } from '@vercel/remix/server'
 import * as build from '@remix-run/dev/server-build'
 import { installGlobals } from '@remix-run/node'
 
 installGlobals()
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV })
+export default createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+  getLoadContext() {
+    return {
+      nodeLoadContext: true,
+    }
+  },
+})
